cbc9dd783057c5a7c25a98e23f9822c70247a7ea
Utku Aydin <30289497+Brodican@users.noreply.github.com>
2020-06-09 08:49:48 +0100

    Added comments, restructured methods.
    Signed-off-by: Utku Aydin <30289497+Brodican@users.noreply.github.com>

--- RockPaperScissors/src/main/java/mthree/rockpaperscissors/RockPaperScissors.java
+++ RockPaperScissors/src/main/java/mthree/rockpaperscissors/RockPaperScissors.java
@@ -13,118 +13,162 @@ import java.util.Scanner;
  * @author utkua
  */
 public class RockPaperScissors {
-
-    static int choice = 0;
-    static int compChoice;
-    static int rounds = 0;
-    static int roundCount = 0;
     
+    // Global scanner used in multiple methods
     static Scanner s;
-    static boolean done;
 
     public static void main(String[] args) {
+        
+        // Store player choice
+        int choice;
+        // Store computer choice
+        int compChoice;
+        // Store player round number input, total number of rounds
+        int rounds;
+        // Store current round
+        int roundCount;
 
+        // Count ties, wins, losses
         int tieCount;
         int winCount;
         int lossCount;
         
+        // Array of moves corresponding to choices
         String[] choices = {"Rock", "Paper", "Scissors"};
-        
-        String input;
-        
+        // Boolean determining if game loop should occur again
         boolean playAgain = true;
-
+        
+        // Random determining computer move choice
         Random r = new Random();
         
+        // Outer game loop, loops once each game
         while (playAgain) {
+            // Reset all counts for new game
             tieCount = 0;
             winCount = 0;
             lossCount = 0;
             roundCount = 0;
             
-            done = false;
-            do {
-                try {
-                    s = new Scanner(System.in);
-                    System.out.println("Input a round count: ");
-                    rounds = s.nextInt();
-                    if ((rounds <= 10) && (rounds >= 1)) {
-                        done = true;
-                    } else {
-                        System.out.println("The round count must be 1 - 10.");
-                        System.exit(0);
-                    }
-                } catch(Exception e) {
-                    System.out.println("The round count must be an integer.");
-                    System.exit(0);
-                }
-            } while(!done);
+            // Get round input from player
+            rounds = ChooseRounds();
             
+            // Inner round loop, loops once per round
             while (roundCount < rounds) {
-                Choose();
-
+                choice = ChooseMove();
                 compChoice = r.nextInt(3) + 1;
+                
+                // If player and computer moves equal, tie
                 if (compChoice == choice) {
-                    System.out.println("Computer choice: " + choices[compChoice - 1] + " your choice " + choices[choice - 1]);
+                    // Print choices, result, and increment tie count to indicate tie
+                    System.out.println("Computer choice: " + choices[compChoice - 1] + ", your choice: " + choices[choice - 1]);
                     System.out.println("Tie");
                     tieCount++;
-                } else if ((choice % 3) == (compChoice + 1) % 3) {
-                    System.out.println("Computer choice: " + choices[compChoice - 1] + " your choice " + choices[choice - 1]);
+                } 
+                // If player choice 1 above computer choice, mod 3
+                // mod 3 because 3 moves
+                else if ((choice % 3) == (compChoice + 1) % 3) {
+                    // Print choices, result, and increment win count to indicate win
+                    System.out.println("Computer choice: " + choices[compChoice - 1] + ", your choice: " + choices[choice - 1]);
                     System.out.println("Win");
                     winCount++;
-                } else {
-                    System.out.println("Computer choice: " + choices[compChoice - 1] + " your choice " + choices[choice - 1]);
+                }
+                // Otherwise computer wins
+                else {
+                    // Print choices, result, and increment loss count to indicate loss
+                    System.out.println("Computer choice: " + choices[compChoice - 1] + ", your choice: " + choices[choice - 1]);
                     System.out.println("Loss");
                     lossCount++;
                 }
+                // Increment to indicate another round is done
                 roundCount++;
             }
             
+            // Print tie, win, and loss count
             System.out.println("Ties: " + tieCount + ".");
             System.out.println("Wins: " + winCount + ".");
             System.out.println("Losses: " + lossCount + ".");
             
-            done = false;
-            do {
-                try {
-                    s = new Scanner(System.in);
-                    System.out.println("Play again? y/n ");
-                    input = s.nextLine();
-                    if (input.equals("y")) {
-                        roundCount = 0;
-                        done = true;
-                        playAgain = true;
-                    } else if (input.equals("n")) {
-                        done = true;
-                        playAgain = false;
-                        System.out.println("Thanks for playing!");
-                    } else {
-                        System.out.println("Input either 'y' or 'n', without quotes." + input);
-                        
-                    }
-                } catch(Exception e) {
-                    System.out.println("Input error.");
-                }
-            } while(!done);
+            // Ask player if they want to play again, playAgain true if so so out loop loops again
+            playAgain = ChooseAgain();;
         }
     }
     
-    // 
-    private static void Choose() {
-        done = false;
+    // Returns move choice input from user
+    private static int ChooseMove() {
+        
+        int choice;
+        
+        // Loop until correct input
         do {
+            // Catch error in case of non-integer input
             try {
                 s = new Scanner(System.in);
                 System.out.println("Input a choice - 1 for Rock, 2 for Paper, 3 for Scissors: ");
                 choice = s.nextInt();
+                // If choice in right range, return choice, otherwise ask again
                 if ((choice <= 3) && (choice >=1)) {
-                    done = true;
+                    return choice;
                 } else {
                     System.out.println("Your choice must be 1 for Rock, 2 for Paper, 3 for Scissors.");
                 }
             } catch(Exception e) {
                 System.out.println("Your choice must be an integer.");
             }
-        } while(!done);
+        } while(true);
+    }
+    
+    // Returns round count input from user
+    private static int ChooseRounds() {
+        
+        int rounds;
+        
+        // Catch error in case of non-integer input
+        try {
+            s = new Scanner(System.in);
+            // Ask for and take input
+            System.out.println("Input a round count: ");
+            rounds = s.nextInt();
+            if ((rounds <= 10) && (rounds >= 1)) {
+                return rounds;
+            } else {
+                // Print error and quit if input out of bounds
+                System.out.println("The round count must be 1 - 10.");
+                System.exit(0);
+            }
+        } catch(Exception e) {
+            System.out.println("The round count must be an integer.");
+            System.exit(0);
+        }
+        
+        return 0;
+    }
+    
+    // Returns player input on their choice to play again
+    // true if they want to play again, false otherwise
+    private static boolean ChooseAgain() {
+        
+        // Loop until correct input
+        do {
+            try {
+                s = new Scanner(System.in);
+                System.out.println("Play again? y/n ");
+                String input = s.nextLine();
+                // If player inputs 'y', return true to indicate they want to play again
+                if (input.equals("y")) {
+                    return true;
+                } 
+                // If player inputs 'n', return false
+                else if (input.equals("n")) {
+                    System.out.println("Thanks for playing!");
+                    return false;
+                } 
+                // Otherwise, wrong input so ask again
+                else {
+                    System.out.println("Input either 'y' or 'n', without quotes.");
+                }
+            } catch(Exception e) {
+                System.out.println("Input format error.");
+            }
+        } while(true);
     }
 }
